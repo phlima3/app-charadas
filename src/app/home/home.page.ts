51b101037846f76b26d969a1457fa03f1a2f8a5d
Pedro Lima <psantoslima86@gmail.com>
2022-10-18 22:15:05 -0300

    main

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pergunta: string;
+  resposta: string;
+  hasClicked = false;
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
-
+  solicitarCharada() {
+    const url = 'http://lucasreno.kinghost.net/charada';
+    this.http.get(url).subscribe((resposta: any) => {
+      this.pergunta = resposta[0].pergunta;
+      this.resposta = resposta[0].resposta;
+    });
+    this.hasClicked = false;
+  }
+  responder() {
+    this.hasClicked = true;
+  }
 }
